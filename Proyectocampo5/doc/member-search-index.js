1d21e3fb5ea869a07a40f6c3607b5c27d0099260
Ricardo <N00321093@upn.pe>
2025-05-04 21:37:47 -0500

    Trabajo de campo numero 5(sobrecarga de metodos )

---
+++ Proyectocampo5/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"campo5","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"campo5","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ Proyectocampo5/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Proyectocampo5","l":"campo5"}];updateSearchResults();
